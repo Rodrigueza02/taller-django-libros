888b4bb0a92bb19a8c59d0b5f1b37aac77446e13
Rodrigueza02 <mariaj.rodrigueza@campusucc.edu.co>
2026-04-23 13:32:24 -0500

    fix: limpiar settings.py variables duplicadas

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-md4&1^0_#ah^fb#_5d%f7k%_9jsr&)wlov8ysv8bljshd7^tof'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+import os
 
-ALLOWED_HOSTS = []
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+
+ALLOWED_HOSTS = ['*', 'taller-django-libros-ico3.onrender.com']
 
 
 # Application definition
@@ -117,13 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# Para que Django sirva archivos estáticos en producción
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# Heroku
-import os
-
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-
-ALLOWED_HOSTS = ['*', 'taller-django-libros-ico3.onrender.com']
